f234aefc9f4d7b47ebdcfe135c57fa58b28378a1
unknown <sanjaych9517@gmail.com>
2026-08-22 18:36:12 +0530

    end npm and tools

--- chapter_7_NPM_and_tools/app.js
+++ chapter_7_NPM_and_tools/app.js
@@ -4,7 +4,7 @@ const server = http.createServer((req, res) =>{
     console.log(req);
 });
 
-const PORT = 3000;
+const PORT = 3001;
 
 server.listen(PORT, ()=>{
     console.log(`server is listening on port${PORT}`);
