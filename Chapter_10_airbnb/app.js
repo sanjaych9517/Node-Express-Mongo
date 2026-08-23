664adea0ae090a739831d4dbd0e19ad73e41d95c
unknown <sanjaych9517@gmail.com>
2026-08-23 11:09:44 +0530

    we create views dir and create files

--- Chapter_10_airbnb/app.js
+++ Chapter_10_airbnb/app.js
@@ -2,8 +2,8 @@
 const express = require('express');
 
 // local module
- const userRoute = require("./routes/userRouter");
- const hostRouter = require("./routes/hostRouter");
+const userRoute = require("./routes/userRouter");
+const hostRouter = require("./routes/hostRouter");
 
 const app = express();
 
@@ -15,10 +15,13 @@ app.use((req, res, next) => {
 app.use(express.urlencoded());
 
 app.use(userRoute);
-app.use(hostRouter);
+app.use( "/host",hostRouter);
 
-
+app.use((req, res, next) =>{
+  res.status(404).send(`<h1>404 Your page is not on airbnb</h1>`);
+})
+ 
 const PORT = 3000;
-app.listen(PORT, () =>{
-    console.log(`Server is running on address http://localhost:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on address http://localhost:${PORT}`);
 });
--- Chapter_10_airbnb/routes/userRouter.js
+++ Chapter_10_airbnb/routes/userRouter.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const userRouter = express.Router();
 
-userRouter.get("/", (req, res, next) =>{
+userRouter.get("/", (req, res, next) => {
   res.send(`<h1>welcome to airbnb </h1>
     <a href="/host/add-home">Add Home</a>
     `)
