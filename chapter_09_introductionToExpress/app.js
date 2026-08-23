98f1ae3bfe777aed61837baf8ed846fbfa9cd15a
unknown <sanjaych9517@gmail.com>
2026-08-23 06:30:54 +0530

    handling request and starting of express complete

---
+++ chapter_09_introductionToExpress/app.js
@@ -0,0 +1,31 @@
+
+// external module
+const express = require('express')
+// Local Module
+const userRequestHandler = require('./user')
+
+const app = express();
+
+app.get("/", (req, res, next) => {
+    console.log( " came in first middle ware",req.url, req.method);
+    // res.send("<p> Came from first middleware:</p>")
+    next();
+});
+
+app.post("/submit-details", (req, res, next) => {
+    console.log( " came in second  middle ware",req.url, req.method);
+    res.send("<p> Welcome to complete coding in Node.js series </p>")
+});
+
+app.use("/", (req, res, next) => {
+    console.log( " came in first middle ware",req.url, req.method);
+    res.send("<p> Came from another middleware:</p>")
+    next();
+});
+
+
+
+const PORT = 3000;
+app.listen(PORT, ()=>{
+    console.log(`server is listening on port http://localhost:${PORT}`);
+})
